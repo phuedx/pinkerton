phuedx/pinkerton

commit b89fdc1795c84ea9b9d02f435cc97959c52d061f

Migrate Pinkerton class from runkit to UOPZ
* Rewrite Pinkerton::spyOn and Pinkerton::stopSpyingOn to use
  uopz_function and uopz_restore respectively
* Remove the Pinkerton::getSpy function as the UOPZ handler closes over
  the spy

src/Phuedx/Pinkerton/Pinkerton.php
@@ -6,48 +6,31 @@ class Pinkerton
 {
     private static $investigations = array();
 
-    public static function getSpy($function)
-    {
-        return self::$investigations[$function]['spy'];
-    }
-
     public static function spyOn($function)
     {
         if ( ! function_exists($function)) {
             throw new \InvalidArgumentException("The {$function} function doesn't exist.");
         }
 
-        $newFunction = uniqid($function);
-        runkit_function_copy($function, $newFunction);
+        $spy = new Spy($function);
+        $handler = function () use ($spy) {
+            $arguments = func_get_args();
 
-        $spy = new Spy($newFunction);
-        self::$investigations[$function] = array(
-            'spy' => $spy,
-            'new_function_name' => $newFunction,
-        );
+            return call_user_func_array($spy, $arguments);
+        };
+        uopz_function($function, $handler);
 
-        $code = <<<'PHP'
-$arguments = func_get_args();
-$spy = \Phuedx\Pinkerton\Pinkerton::getSpy('%s');
-
-return call_user_func_array($spy, $arguments);
-PHP;
-
-        runkit_function_redefine($function, '', sprintf($code, $function));
+        self::$investigations[] = $function;
 
         return $spy;
     }
 
     public static function stopSpyingOn($function)
     {
-        if ( ! isset(self::$investigations[$function])) {
+        if ( ! in_array($function, self::$investigations)) {
             throw new \InvalidArgumentException("The {$function} function isn't being spied on.");
         }
 
-        $investigation = self::$investigations[$function];
-
-        runkit_function_remove($function);
-        runkit_function_copy($investigation['new_function_name'], $function);
-        runkit_function_remove($investigation['new_function_name']);
+        uopz_restore($function);
     }
 }

commit 27c34f6c74a201991122fe602c9d48be4c20c8d5

Manually back up functions (fixes GH-4)
The spy returned by Pinkerton::spy should wrap a copy of the original
function or method because it's replaced by a handler that invokes the
spy. Because of this, spyOn( ... )->andCallThrough followed by an
invocation segfaults.

This regression was first introduced in b89fdc1.

src/Phuedx/Pinkerton/Pinkerton.php
@@ -25,7 +25,10 @@ class Pinkerton
             throw new \InvalidArgumentException("The {$methodString} method doesn't exist.");
         }
 
-        $spy = new Spy($function);
+        $newMethodName = uniqid($method, true);
+        uopz_rename($className, $method, $newMethodName);
+
+        $spy = new Spy([$class, $newMethodName]);
         $handler = self::createHandler($spy);
         uopz_function($className, $method, $handler);
 
@@ -40,7 +43,10 @@ class Pinkerton
             throw new \InvalidArgumentException("The {$function} function doesn't exist.");
         }
 
-        $spy = new Spy($function);
+        $newFunctionName = uniqid($function, true);
+        uopz_rename($function, $newFunctionName);
+
+        $spy = new Spy($newFunctionName);
         $handler = self::createHandler($spy);
         uopz_function($function, $handler);
 